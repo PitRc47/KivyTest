5303856c5ee6e9f69726fa1a5ec4c5a12d3d1b00
PitRc47 <189371188+PitRc47@users.noreply.github.com>
2025-02-05 11:04:47 +0800

    test: pygame

--- main.py
+++ main.py
@@ -2,6 +2,30 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 from kivy.core.audio import SoundLoader
+def toDowngradeAPI():
+    global mixer
+    
+    if checksys.main != 'Android':
+        from os import environ; environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
+        from pygame import mixer as _mixer
+        _mixer.Sound
+        
+        _mixer.init()
+        mixer = _mixer
+        
+        length = -1
+        _load = mixer.music.load
+        _get_pos = mixer.music.get_pos
+        
+        def _loadhook(fn: str):
+            nonlocal length
+            
+            length = _mixer.Sound(fn).get_length()
+            _load(fn)
+            
+        mixer.music.load = _loadhook
+        mixer.music.get_length = lambda: length
+        mixer.music.get_pos = lambda: _get_pos() / 1000
 
 class MusicGameWidget(Widget):
     def __init__(self, **kwargs):
@@ -16,6 +40,7 @@ class MusicGameWidget(Widget):
 
 class MusicGameApp(App):
     def build(self):
+        toDowngradeAPI()
         return MusicGameWidget()
 
 if __name__ == '__main__':
